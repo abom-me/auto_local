39231a31ebf8a97b3b512781e707252619a55e8a
Nasr Al-Rahbi <nasr@abom.me>
2024-07-20 12:44:17 +0400

    added

--- bin/auto_local.dart
+++ bin/auto_local.dart
@@ -1,7 +1,18 @@
-import 'package:auto_local/functions.dart';
+import 'package:args/command_runner.dart';
+import 'package:auto_local/icons/icon.dart';
+import 'package:auto_local/lang/lang.dart';
+import 'package:auto_local/print_color.dart';
 
-void main(List<String> arguments) {
-AutoLocal autoLocal = AutoLocal();
-// autoLocal.firstRun();
-autoLocal.run();
+void main(List<String> args) async {
+  Print pen = Print();
+  pen.green(
+      "|------------------👋 Starting Auto Local Tool 👋----------------------------|");
+  pen.green(
+      "|                   This Tool Built by Github: abom-me                                 |");
+  final runner = CommandRunner('auto_local', "")
+    ..addCommand(Lang())
+    ..addCommand(AutoIcon());
+  // runner.argParser.addOption('char', help: 'The character to use for drawing');
+  await runner.run(args);
 }
+//
---
+++ lib/commands.dart
@@ -0,0 +1,38 @@
+// import 'package:auto_local/functions.dart';
+// import 'package:auto_local/icons/icon.dart';
+// import 'package:auto_local/print_color.dart';
+//
+// import 'lang/lang.dart';
+//
+// class Commands {
+//    List<String> commands = [];
+//    Commands(this.commands);
+//    Print pen =Print();
+//    AutoLocal autoLocal = AutoLocal();
+//    Lang langClass = Lang();
+//
+// lang(){
+//   int length = commands.length;
+//   if(length>1){
+//
+//  if(length>2){
+//    switch (commands[2]) {
+//      case '--add':
+//        // langClass.addText(commands[1]);
+//        break;
+//      case '--get':
+//      // autoIcon.run();
+//        break;
+//      default:
+//        pen.red( "Invalid command. Usage: auto_local <lang> \"Text\" [ --add | --get]");
+//    }
+//  }else{
+//    pen.red( "Invalid command. Usage: auto_local <lang> \"Text\" [ --add | --get]");
+//  }
+//
+//   }else{
+//     autoLocal.run();
+//   }
+//
+// }
+// }
